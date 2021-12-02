6efc1596f616bec2edc480ce5ba693eb8bf9b86e
Alaricode <antonlarichev@gmail.com>
2021-12-02 19:02:46 +0300

    Tuples

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
 "use strict";
-const skills = ['Dev', 'DevOps', 'Testing'];
-for (const skill of skills) {
-    console.log(skill.toLowerCase());
-}
-const res = skills
-    .filter((s) => s !== 'DevOps')
-    .map(s => s + '! ')
-    .reduce((a, b) => a + b);
-console.log(res);
+const skill = [1, 'Dev'];
--- app.ts
+++ app.ts
@@ -1,12 +1,5 @@
-const skills: string[] = ['Dev', 'DevOps', 'Testing'];
+const skill: [number, string] = [1, 'Dev'];
 
-for (const skill of skills) {
-	console.log(skill.toLowerCase());
-}
+const [id, skillName] = skill;
 
-const res = skills
-	.filter((s: string) => s !== 'DevOps')
-	.map(s => { return 1 })
-	.reduce((a, b) => a + b);
-
-console.log(res);
+const arr: [number, string, ...boolean[]] = [1, 'sdf', true, true]
