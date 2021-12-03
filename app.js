64bc76f38ea6c2d82f7c9f4d560b68f5a5e307a3
Alaricode <antonlarichev@gmail.com>
2021-12-03 15:31:15 +0300

    Enums

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 "use strict";
-const skill = [1, 'Dev'];
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 1] = "SUCCESS";
+    StatusCode["IN_PROCESS"] = "p";
+    StatusCode["FAILED"] = "f";
+})(StatusCode || (StatusCode = {}));
+const res = {
+    message: 'Платёж успешен',
+    statusCode: StatusCode.SUCCESS
+};
+// 1 - успех
+// 'p' - в процессе
+// 'f' - отклонён
+if (res.statusCode === StatusCode.SUCCESS) {
+}
+function action(status) {
+}
+action(StatusCode.SUCCESS);
+action(1);
+const res2 = 1 /* ADMIN */;
--- app.ts
+++ app.ts
@@ -1,3 +1,33 @@
-const skill: readonly [number, string] = [1, 'Dev'];
+enum StatusCode {
+	SUCCESS = 1,
+	IN_PROCESS = 'p',
+	FAILED = 'f'
+}
 
-const skills: ReadonlyArray<string> = ['Dev', 'DevOps'];
+const res = {
+	message: 'Платёж успешен',
+	statusCode: StatusCode.SUCCESS
+};
+
+
+// 1 - успех
+// 'p' - в процессе
+// 'f' - отклонён
+
+if (res.statusCode === StatusCode.SUCCESS) {
+
+}
+
+function action(status: StatusCode) {
+
+}
+
+action(StatusCode.SUCCESS);
+action(1);
+
+const enum Roles {
+	ADMIN = 1,
+	USER = 2
+}
+
+const res2 = Roles.ADMIN;
