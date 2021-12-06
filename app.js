79b5865c4fa8b4b0a80d81d0cd2caa20d1f890db
Alaricode <antonlarichev@gmail.com>
2021-12-06 16:28:24 +0300

    Literal Types

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
 "use strict";
-var StatusCode;
-(function (StatusCode) {
-    StatusCode[StatusCode["SUCCESS"] = 1] = "SUCCESS";
-    StatusCode["IN_PROCESS"] = "p";
-    StatusCode["FAILED"] = "f";
-})(StatusCode || (StatusCode = {}));
-const res = {
-    message: 'Платёж успешен',
-    statusCode: StatusCode.SUCCESS
-};
-// 1 - успех
-// 'p' - в процессе
-// 'f' - отклонён
-if (res.statusCode === StatusCode.SUCCESS) {
+function fetchWithAuth(url, method) {
+    return 1;
 }
-function action(status) {
-}
-action(StatusCode.SUCCESS);
-action(1);
-const res2 = 1 /* ADMIN */;
+fetchWithAuth('s', 'post');
+let method = 'post';
+fetchWithAuth('s', method);
--- app.ts
+++ app.ts
@@ -1,36 +1,9 @@
-function logId(id: string | number | boolean) {
-	if (typeof id === 'string') {
-		console.log(id);
-	} else if (typeof id === 'number') {
-		console.log(id);
-	} else {
-		console.log(id);
-	}
+function fetchWithAuth(url: string, method: 'post' | 'get'): 1 | -1 {
+	return 1;
 }
 
-function logError(err: string | string[]) {
-	if (Array.isArray(err)) {
-		console.log(err);
-	} else {
-		console.log(err);
-	}
-}
+fetchWithAuth('s', 'post');
 
-function logObject(obj: { a: number } | { b: number }) {
-	if ('a' in obj) {
-		console.log(obj.a);
-	} else {
-		console.log(obj.b);
-	}
-}
+let method = 'post';
 
-function logMultipleIds(a: string | number, b: string | boolean) {
-	if (a === b) {
-
-	} else {
-		console.log(a);
-	}
-}
-
-let a: 1 = 1;
-a = 2
+fetchWithAuth('s', method as 'post');
