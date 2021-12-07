461bc1d1e5080d671ce984272870f3af27d79b75
Alaricode <antonlarichev@gmail.com>
2021-12-07 15:45:21 +0300

    Types или Interfaces

--- app.ts
+++ app.ts
@@ -1,44 +1,12 @@
 interface User {
-	name: string,
-	age: number,
-	skills: string[]
-
-	log: (id: number) => string;
+	name: string
 }
 
-interface Role {
-	roleId: number;
+interface User {
+	age: number;
 }
 
-interface UserWithRole extends User, Role {
-	createdAt: Date;
-}
-
-type User2 = {
-	name: string,
-	age: number,
-	skills: string[]
-
-	log: (id: number) => string;
-}
-
-
-let user: UserWithRole = {
-	name: 'asd',
-	age: 33,
-	skills: ['1', '2'],
-	roleId: 1,
-	createdAt: new Date(),
-
-	log(id) {
-		return '';
-	}
-};
-
-interface UserDic {
-	[index: string]: User
-}
-
-type UserDic2 = {
-	[index: number]: User
+const user: User = {
+	name: 'as',
+	age: 33
 }
