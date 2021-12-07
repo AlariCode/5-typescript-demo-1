358e6dbff86c04ec3419e5784852d21a8081402b
Alaricode <antonlarichev@gmail.com>
2021-12-07 16:15:34 +0300

    Optional

--- app.ts
+++ app.ts
@@ -1,12 +1,18 @@
 interface User {
-	name: string
-}
-
-interface User {
-	age: number;
+	login: string;
+	password?: string;
 }
 
 const user: User = {
-	name: 'as',
-	age: 33
+	login: 'a@a.ru',
+	password: '1'
 }
+
+function multiply(first: number, second?: number): number {
+	if (!second) {
+		return first * first;
+	}
+	return first * second;
+}
+
+multiply(5)
