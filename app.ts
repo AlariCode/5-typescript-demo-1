a070efad4bc0bab754bc51e4d1cde3a5e2bd2999
Alaricode <antonlarichev@gmail.com>
2021-12-13 13:51:18 +0300

    Type Guard

--- app.ts
+++ app.ts
@@ -1,11 +1,3 @@
-let a = 5;
-let b: string = a.toString()
-let e: string = new String(a).valueOf()
-let f: boolean = new Boolean(a).valueOf()
-
-let c = 'sdfs';
-let d: number = parseInt(c);
-
 interface User {
 	name: string;
 	email: string;
@@ -23,14 +15,30 @@ interface Admin {
 	role: number;
 }
 
-const admin: Admin = {
-	...user,
-	role: 1
+function logId(id: string | number) {
+	if (isString(id)) {
+		console.log(id);
+	} else {
+		console.log(id);
+	}
 }
 
-function userToAdmin(user: User): Admin {
-	return {
-		name: user.login,
-		role: 1
+function isString(x: string | number): x is string {
+	return typeof x === 'string';
+}
+
+function isAdmin(user: User | Admin): user is Admin {
+	return 'role' in user;
+}
+
+function isAdminAlternative(user: User | Admin): user is Admin {
+	return (user as Admin).role !== undefined;
+}
+
+function setRoleZero(user: User | Admin) {
+	if (isAdmin(user)) {
+		user.role = 0;
+	} else {
+		throw new Error('Пользователь не админ');
 	}
 }
