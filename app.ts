63b471d601fbccdbedab1bca2e6b9838e1e2d95e
Alaricode <antonlarichev@gmail.com>
2021-12-16 18:23:28 +0300

    Implements

--- app.ts
+++ app.ts
@@ -1,27 +1,25 @@
-class User {
-	_login: string;
-	password: string;
-	createdAt: Date;
-
-	set login(l: string | number) {
-		this._login = 'user-' + l;
-		this.createdAt = new Date();
-	}
-
-	get login() {
-		return this._login;
-	}
-
-	async getPassword(p: string) {
-
-	}
-
-	// set password(p: string) {
-	// 	// sync
-	// }
+interface ILogger {
+	log(...args): void;
+	error(...args): void;
 }
 
-const user = new User();
-user.login = 'myLogin';
-console.log(user);
-console.log(user.login);
+class Looger implements ILogger {
+	log(...args: any[]): void {
+		console.log(...args);
+	}
+	async error(...args: any[]): Promise<void> {
+		// Кинуть во внешнюю систему
+		console.log(...args);
+	}
+}
+
+interface IPayable {
+	pay(paymentId: number): void;
+	price?: number;
+}
+
+class User implements IPayable {
+	pay(paymentId: number | string): void {
+		///
+	}
+}
