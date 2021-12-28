a4e8bcd6611ebc300041041dc558e6f01d1c8692
Alaricode <antonlarichev@gmail.com>
2021-12-28 17:55:34 +0300

    Пример встроенных generic

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,12 @@
-class User {
-	role?: 'admin' | 'user';
+const num: Array<number> = [1, 2, 3];
 
-	constructor(private name: string) {
-	}
-
-	login(): void { }
+async function test() {
+	const a = await new Promise<number>((resolve, reject) => {
+		resolve(1);
+	});
 }
 
-function createUser(user: User) {
-	// логика
-	const defaultUser = new User('defauilt');
-	defaultUser.role = undefined;
-
-	switch (user.role) {
-		case 'admin':
-			const a = 7;
-		case 'user':
-			return true;
-			const c = 1;
-	}
-}
-
-interface IChecks {
-	[check: string]: boolean;
-}
-const c: IChecks = { kpp: true };
-const d = c['drive'];
-c.drive
-
-class VipUser extends User {
-	override login(): void {
-
-	}
-}
+const check: Record<string, boolean> = {
+	drive: true,
+	kpp: false
+};
