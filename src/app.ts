0b44184c849952117d9249cac33833115dbe2172
Alaricode <antonlarichev@gmail.com>
2021-12-28 18:10:29 +0300

    Пишем функцию с generic

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
-const num: Array<number> = [1, 2, 3];
-
-async function test() {
-	const a = await new Promise<number>((resolve, reject) => {
-		resolve(1);
-	});
+function logMiddleware<T>(data: T): T {
+	console.log(data);
+	return data;
 }
 
-const check: Record<string, boolean> = {
-	drive: true,
-	kpp: false
-};
+const res = logMiddleware<number>(10);
+
+function getSplitedHalf<T>(data: Array<T>): Array<T> {
+	const l = data.length / 2;
+	return data.splice(0, l);
+}
+
+getSplitedHalf<number>([1, 3, 4]);
