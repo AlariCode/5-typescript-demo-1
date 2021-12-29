3609ee3e22bf72651c22c3d8de78951aae19054b
Alaricode <antonlarichev@gmail.com>
2021-12-29 12:33:38 +0300

    Generic классы

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,25 @@
-interface Vehicle {
-	run: number;
+class Resp<D, E> {
+	data?: D;
+	error?: E;
+
+	constructor(data?: D, error?: E) {
+		if (data) {
+			this.data = data;
+		}
+		if (error) {
+			this.error = error;
+		}
+	}
 }
 
-function kmToMiles<T extends Vehicle>(vehicle: T): T {
-	vehicle.run = vehicle.run / 0.62;
-	return vehicle;
+const res = new Resp<string, number>('data');
+
+class HTTPResp<F> extends Resp<string, number>  {
+	code: F;
+
+	setCode(code: F) {
+		this.code = code;
+	}
 }
 
-interface LCV extends Vehicle {
-	capacity: number;
-}
-
-// const vehicle = kmToMiles(new Vehicle());
-// const lcv = kmToMiles(new LCV());
-kmToMiles({ run: 1 });
-
-function logId<T extends string | number, Y>(id: T, additionalData: Y): { id: T, data: Y } {
-	console.log(id);
-	console.log(additionalData);
-	return { id, data: additionalData };
-}
+const res2 = new HTTPResp();
