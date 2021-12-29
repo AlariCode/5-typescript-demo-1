28d352576cf1a79be142275b88169317216c0ef1
Alaricode <antonlarichev@gmail.com>
2021-12-29 18:26:17 +0300

    Keyof

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,19 @@
-type Constructor = new (...args: any[]) => {}
-type GConstructor<T = {}> = new (...args: any[]) => T
-
-class List {
-	constructor(public items: string[]) { }
+interface IUser {
+	name: string;
+	age: number;
 }
 
-class Accordion {
-	isOpened: boolean;
+type KeysOfUser = keyof IUser;
+
+const key: KeysOfUser = 'age';
+
+function getValue<T, K extends keyof T>(obj: T, key: K) {
+	return obj[key];
 }
 
-type ListType = GConstructor<List>;
-type AccordionType = GConstructor<Accordion>;
+const user: IUser = {
+	name: 'Вася',
+	age: 30
+};
 
-class ExtendedListClass extends List {
-	first() {
-		return this.items[0];
-	}
-}
-
-function ExtendedList<TBase extends ListType & AccordionType>(Base: TBase) {
-	return class ExtendedList extends Base {
-		first() {
-			return this.items[0];
-		}
-	}
-}
-
-class AccordionList {
-	isOpened: boolean;
-	constructor(public items: string[]) { }
-}
-
-const list = ExtendedList(AccordionList);
-const res = new list(['first', 'second']);
-console.log(res.first());
+const userName = getValue(user, 'name');
