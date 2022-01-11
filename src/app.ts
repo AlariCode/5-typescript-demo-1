3ac53574d4c020e47cbfd000d3d4e5ef5e24e1a6
Alaricode <antonlarichev@gmail.com>
2022-01-11 15:43:04 +0300

    Mapped Types

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
-function runTransaction(transaction: {
-	fromTo: [string, string]
-}) {
-	console.log(transaction);
+type Modifier = 'read' | 'update' | 'create';
+
+type UserRoles = {
+	customers?: Modifier,
+	projects?: Modifier,
+	adminPanel?: Modifier,
 }
 
-const transaction: GetFirstArg<typeof runTransaction> = {
-	fromTo: ['1', '2']
+type ModifierToAccess<Type> = {
+	+readonly [Property in keyof Type as Exclude<`canAccess${string & Property}`, 'canAccessadminPanel'>]-?: boolean;
 }
 
-runTransaction(transaction);
+type UserAccess2 = ModifierToAccess<UserRoles>;
 
-type GetFirstArg<T> = T extends (first: infer First, ...args: any[]) => any ? First : never;
+
+type UserAccess1 = {
+	customers?: boolean,
+	projects?: boolean,
+	adminPanel?: boolean,
+}
