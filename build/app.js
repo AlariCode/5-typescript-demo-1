67c8363d92f776cae3cc63e4e1e858891d32bb8b
Alaricode <antonlarichev@gmail.com>
2022-01-21 15:06:46 +0300

    Декоратор метода

--- build/app.js
+++ build/app.js
@@ -5,57 +5,25 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-// @nullUser
-let UserService = 
-// @threeUserAdvanced
-// @setUserAdvanced(4)
 class UserService {
     constructor() {
         this.users = 1000;
     }
     getUsersInDatabase() {
-        return this.users;
+        throw new Error('Ошибка');
     }
-};
-UserService = __decorate([
-    log(),
-    setUsers(2)
-    // @threeUserAdvanced
-    // @setUserAdvanced(4)
-], UserService);
-function nullUser(target) {
-    target.prototype.users = 0;
 }
-function setUsers(users) {
-    console.log('setUsers init');
-    return (target) => {
-        console.log('setUsers run');
-        target.prototype.users = users;
-    };
-}
-function log() {
-    console.log('log init');
-    return (target) => {
-        console.log('log run');
+__decorate([
+    Log()
+], UserService.prototype, "getUsersInDatabase", null);
+function Log() {
+    return (target, propertyKey, descriptor) => {
         console.log(target);
-    };
-}
-function setUserAdvanced(users) {
-    return (constructor) => {
-        return class extends constructor {
-            constructor() {
-                super(...arguments);
-                this.users = users;
-            }
+        console.log(propertyKey);
+        console.log(descriptor);
+        descriptor.value = () => {
+            console.log('no error');
         };
-    };
-}
-function threeUserAdvanced(constructor) {
-    return class extends constructor {
-        constructor() {
-            super(...arguments);
-            this.users = 3;
-        }
     };
 }
 console.log(new UserService().getUsersInDatabase());
--- src/app.ts
+++ src/app.ts
@@ -3,50 +3,27 @@ interface IUserService {
 	getUsersInDatabase(): number;
 }
 
-// @nullUser
-@log()
-@setUsers(2)
-// @threeUserAdvanced
-// @setUserAdvanced(4)
 class UserService implements IUserService {
 	users: number = 1000;
 
+	@Log()
 	getUsersInDatabase(): number {
-		return this.users;
+		throw new Error('Ошибка');
 	}
 }
 
-function nullUser(target: Function) {
-	target.prototype.users = 0;
-}
-
-function setUsers(users: number) {
-	console.log('setUsers init');
-	return (target: Function) => {
-		console.log('setUsers run');
-		target.prototype.users = users;
-	}
-}
-
-function log() {
-	console.log('log init');
-	return (target: Function) => {
-		console.log('log run');
+function Log() {
+	return (
+		target: Object,
+		propertyKey: string | symbol,
+		descriptor: TypedPropertyDescriptor<(...args: any[]) => any>
+	): TypedPropertyDescriptor<(...args: any[]) => any> | void => {
 		console.log(target);
-	}
-}
-
-function setUserAdvanced(users: number) {
-	return <T extends { new(...args: any[]): {} }>(constructor: T) => {
-		return class extends constructor {
-			users = users;
+		console.log(propertyKey);
+		console.log(descriptor);
+		descriptor.value = () => {
+			console.log('no error');
 		}
-	}
-}
-
-function threeUserAdvanced<T extends { new(...args: any[]): {} }>(constructor: T) {
-	return class extends constructor {
-		users = 3;
 	}
 }
 
