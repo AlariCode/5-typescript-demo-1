27e866aca04b52da5292720ef9a29cc4f0e45a0c
Alaricode <antonlarichev@gmail.com>
2022-01-21 11:35:57 +0300

    Фабрика декораторов

--- build/app.js
+++ build/app.js
@@ -5,7 +5,11 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-let UserService = class UserService {
+// @nullUser
+let UserService = 
+// @threeUserAdvanced
+// @setUserAdvanced(4)
+class UserService {
     constructor() {
         this.users = 1000;
     }
@@ -14,11 +18,37 @@ let UserService = class UserService {
     }
 };
 UserService = __decorate([
-    nullUser,
-    threeUserAdvanced
+    log(),
+    setUsers(2)
+    // @threeUserAdvanced
+    // @setUserAdvanced(4)
 ], UserService);
 function nullUser(target) {
     target.prototype.users = 0;
+}
+function setUsers(users) {
+    console.log('setUsers init');
+    return (target) => {
+        console.log('setUsers run');
+        target.prototype.users = users;
+    };
+}
+function log() {
+    console.log('log init');
+    return (target) => {
+        console.log('log run');
+        console.log(target);
+    };
+}
+function setUserAdvanced(users) {
+    return (constructor) => {
+        return class extends constructor {
+            constructor() {
+                super(...arguments);
+                this.users = users;
+            }
+        };
+    };
 }
 function threeUserAdvanced(constructor) {
     return class extends constructor {
--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ interface IUserService {
 	getUsersInDatabase(): number;
 }
 
-@nullUser
-@threeUserAdvanced
+// @nullUser
+@log()
+@setUsers(2)
+// @threeUserAdvanced
+// @setUserAdvanced(4)
 class UserService implements IUserService {
 	users: number = 1000;
 
@@ -15,6 +18,30 @@ class UserService implements IUserService {
 
 function nullUser(target: Function) {
 	target.prototype.users = 0;
+}
+
+function setUsers(users: number) {
+	console.log('setUsers init');
+	return (target: Function) => {
+		console.log('setUsers run');
+		target.prototype.users = users;
+	}
+}
+
+function log() {
+	console.log('log init');
+	return (target: Function) => {
+		console.log('log run');
+		console.log(target);
+	}
+}
+
+function setUserAdvanced(users: number) {
+	return <T extends { new(...args: any[]): {} }>(constructor: T) => {
+		return class extends constructor {
+			users = users;
+		}
+	}
 }
 
 function threeUserAdvanced<T extends { new(...args: any[]): {} }>(constructor: T) {
