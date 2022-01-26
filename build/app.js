8ad3069ae69eaec48685fb219c406382738c4f49
Alaricode <antonlarichev@gmail.com>
2022-01-26 13:11:19 +0300

    Фабрика

--- build/app.js
+++ build/app.js
@@ -1,6 +1,58 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const really_relaxed_json_1 = require("really-relaxed-json");
-const rjson = '[ one two three {foo:bar} ]';
-const json = (0, really_relaxed_json_1.toJson)(rjson);
-console.log(json);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+class TFInsurance {
+    setVehicle(vehicle) {
+        this.vehicle = vehicle;
+    }
+    submit() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const res = yield fetch('', {
+                method: 'POST',
+                body: JSON.stringify({ vehicle: this.vehicle })
+            });
+            const data = yield res.json();
+            return data.isSuccess;
+        });
+    }
+}
+class ABInsurance {
+    setVehicle(vehicle) {
+        this.vehicle = vehicle;
+    }
+    submit() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const res = yield fetch('ab', {
+                method: 'POST',
+                body: JSON.stringify({ vehicle: this.vehicle })
+            });
+            const data = yield res.json();
+            return data.yes;
+        });
+    }
+}
+class InsuranceFactory {
+    saveHistory(ins) {
+        this.db.save(ins.id, ins.status);
+    }
+}
+class TFInsuranceFactory extends InsuranceFactory {
+    createInsurance() {
+        return new TFInsurance();
+    }
+}
+class ABInsuranceFactory extends InsuranceFactory {
+    createInsurance() {
+        return new ABInsurance();
+    }
+}
+const tfInsuranceFactory = new TFInsuranceFactory();
+const ins = tfInsuranceFactory.createInsurance();
+tfInsuranceFactory.saveHistory(ins);
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,72 @@
-import { toJson } from 'really-relaxed-json'
-const rjson = '[ one two three {foo:bar} ]'
-const json = toJson(rjson)
-console.log(json);
+interface IInsurance {
+	id: number;
+	status: string;
+	setVehicle(vehicle: any): void;
+	submit(): Promise<boolean>;
+}
+
+class TFInsurance implements IInsurance {
+	id: number;
+	status: string;
+	private vehicle: any;
+
+	setVehicle(vehicle: any): void {
+		this.vehicle = vehicle;
+	}
+
+	async submit(): Promise<boolean> {
+		const res = await fetch('',
+			{
+				method: 'POST',
+				body: JSON.stringify({ vehicle: this.vehicle })
+			});
+		const data = await res.json();
+		return data.isSuccess;
+	}
+}
+
+class ABInsurance implements IInsurance {
+	id: number;
+	status: string;
+	private vehicle: any;
+
+	setVehicle(vehicle: any): void {
+		this.vehicle = vehicle;
+	}
+
+	async submit(): Promise<boolean> {
+		const res = await fetch('ab',
+			{
+				method: 'POST',
+				body: JSON.stringify({ vehicle: this.vehicle })
+			});
+		const data = await res.json();
+		return data.yes;
+	}
+}
+
+abstract class InsuranceFactory {
+	db: any;
+
+	abstract createInsurance(): IInsurance;
+
+	saveHistory(ins: IInsurance) {
+		this.db.save(ins.id, ins.status);
+	}
+}
+
+class TFInsuranceFactory extends InsuranceFactory {
+	createInsurance(): TFInsurance {
+		return new TFInsurance();
+	}
+}
+
+class ABInsuranceFactory extends InsuranceFactory {
+	createInsurance(): ABInsurance {
+		return new ABInsurance();
+	}
+}
+
+const tfInsuranceFactory = new TFInsuranceFactory();
+const ins = tfInsuranceFactory.createInsurance();
+tfInsuranceFactory.saveHistory(ins);
