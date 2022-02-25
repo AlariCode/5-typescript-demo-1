563de27f77fcd013374fbb3246fc97907b27b720
Alaricode <antonlarichev@gmail.com>
2022-02-25 18:47:26 +0300

    Asserts

--- build/app.js
+++ build/app.js
@@ -1,52 +1,8 @@
 "use strict";
-class Lead {
-    constructor(name, phone) {
-        this.name = name;
-        this.phone = phone;
+function assertUser(obj) {
+    if (typeof obj === 'object' && !!obj && 'name' in obj) {
+        return;
     }
+    throw new Error('Пользователь не админ');
 }
-class NewLead {
-    constructor() {
-        this.observers = [];
-    }
-    attach(observer) {
-        if (this.observers.includes(observer)) {
-            return;
-        }
-        this.observers.push(observer);
-    }
-    detach(observer) {
-        const observerIndex = this.observers.indexOf(observer);
-        if (observerIndex == -1) {
-            return;
-        }
-        this.observers.splice(observerIndex, 1);
-    }
-    notify() {
-        for (const observer of this.observers) {
-            observer.update(this);
-        }
-    }
-}
-class NotificationService {
-    update(subject) {
-        console.log(`NotificationService получил уведомление`);
-        console.log(subject);
-    }
-}
-class LeadService {
-    update(subject) {
-        console.log(`LeadService получил уведомление`);
-        console.log(subject);
-    }
-}
-const subject = new NewLead();
-subject.state = new Lead('Антон', '00000');
-const s1 = new NotificationService();
-const s2 = new LeadService();
-subject.attach(s1);
-subject.attach(s2);
-subject.notify();
-subject.detach(s1);
-subject.detach(s2);
-subject.notify();
+assertUser({});
--- src/app.ts
+++ src/app.ts
@@ -1,64 +1,23 @@
-interface Observer {
-	update(subject: Subject): void;
+interface User {
+	name: string;
 }
 
-interface Subject {
-	attach(observer: Observer): void
-	detach(observer: Observer): void
-	notify(): void;
-}
+const a = {};
 
-class Lead {
-	constructor(public name: string, public phone: string) { }
-}
+assertUser(a);
+a.name = 'Вася';
 
-class NewLead implements Subject {
-	private observers: Observer[] = [];
-	public state: Lead;
 
-	attach(observer: Observer): void {
-		if (this.observers.includes(observer)) {
-			return;
-		}
-		this.observers.push(observer);
+function assertUser(obj: unknown): asserts obj is User {
+	if (typeof obj === 'object' && !!obj && 'name' in obj) {
+		return;
 	}
-	detach(observer: Observer): void {
-		const observerIndex = this.observers.indexOf(observer);
-		if (observerIndex == -1) {
-			return;
-		}
-		this.observers.splice(observerIndex, 1);
-	}
-
-	notify(): void {
-		for (const observer of this.observers) {
-			observer.update(this);
-		}
-	}
+	throw new Error('Не пользователь');
 }
 
-class NotificationService implements Observer {
-	update(subject: Subject): void {
-		console.log(`NotificationService получил уведомление`);
-		console.log(subject);
+function isUser(obj: unknown): obj is User {
+	if (typeof obj === 'object' && !!obj && 'name' in obj) {
+		return true;
 	}
+	return false;
 }
-
-class LeadService implements Observer {
-	update(subject: Subject): void {
-		console.log(`LeadService получил уведомление`);
-		console.log(subject);
-	}
-}
-
-const subject = new NewLead();
-subject.state = new Lead('Антон', '00000');
-const s1 = new NotificationService();
-const s2 = new LeadService();
-
-subject.attach(s1);
-subject.attach(s2);
-subject.notify();
-subject.detach(s1);
-subject.detach(s2);
-subject.notify();
